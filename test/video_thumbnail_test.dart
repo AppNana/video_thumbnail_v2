dd038e3f1c5cf7c2cf776886db9762358b6f5d7a
xiaomin.liu <395911263@qq.com>
2024-10-18 13:41:13 +0800

    support web

--- test/video_thumbnail_test.dart
+++ test/video_thumbnail_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:get_thumbnail_video/index.dart';
+import 'package:get_thumbnail_video/video_thumbnail.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('video_thumbnail');
